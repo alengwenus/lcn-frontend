29ff663a5f605d773f73cd61548920e396999bcb
Andre Lengwenus <alengwenus@gmail.com>
2024-01-20 07:34:17 +0000

    Fix back-path of entities page

--- src/lcn-entities-page.ts
+++ src/lcn-entities-page.ts
@@ -57,6 +57,7 @@ export class LCNEntitiesPage extends LitElement {
         .narrow=${this.narrow}
         .route=${this.route}
         .tabs=${this.tabs}
+        back-path="/lcn/devices"
       >
         <span slot="header"> ${this.lcn.localize("dashboard-entities-title")} </span>
         <ha-config-section .narrow=${this.narrow}>
