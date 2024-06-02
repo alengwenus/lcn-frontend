214d6ef6e5b2b286dd4df4b3b0569c20485bb4e9
Andre Lengwenus <alengwenus@gmail.com>
2024-06-02 13:22:26 +0000

    Host as URL-param

--- src/lcn-router.ts
+++ src/lcn-router.ts
@@ -2,7 +2,8 @@ import { customElement, property, state } from "lit/decorators";
 import { HassRouterPage, RouterOptions } from "@ha/layouts/hass-router-page";
 import type { HomeAssistant, Route } from "@ha/types";
 import { LCNLogger } from "lcn-logger";
-import { LCN, fetchHosts, LcnHost } from "./types/lcn";
+import { LCN } from "./types/lcn";
+import { getConfigEntry } from "@ha/data/config_entries";
 
 const logger = new LCNLogger("router");
 
@@ -16,7 +17,7 @@ class LCNRouter extends HassRouterPage {
 
   @property({ type: Boolean }) public narrow!: boolean;
 
-  @state() private hosts!: LcnHost[];
+  @state() private _searchParms = new URLSearchParams(window.location.search);
 
   protected routerOptions: RouterOptions = {
     defaultPage: "devices",
@@ -37,7 +38,7 @@ class LCNRouter extends HassRouterPage {
         },
       },
     },
-    initialLoad: () => this._fetchHosts(),
+    initialLoad: () => this._fetchHost(this._searchParms.get("config_entry")!),
   };
 
   protected updatePageEl(el): void {
@@ -46,15 +47,15 @@ class LCNRouter extends HassRouterPage {
     el.route = this.routeTail;
     el.narrow = this.narrow;
 
-    if (this._currentPage === "devices") {
-      el.hosts = this.hosts;
-    }
-
     logger.debug(`Current Page: ${this._currentPage} Route: ${this.route.path}`);
   }
 
-  private async _fetchHosts() {
-    this.hosts = await fetchHosts(this.hass!);
+  private async _fetchHost(entry_id: string) {
+    const res = await getConfigEntry(this.hass!, entry_id);
+    this.lcn.host = {
+      name: res.config_entry.title,
+      id: res.config_entry.entry_id
+    };
   }
 }
 
