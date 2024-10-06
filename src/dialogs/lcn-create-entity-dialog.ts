4002c19af85267a08d7856331560a8fc19ac8f83
Andre Lengwenus <alengwenus@gmail.com>
2024-10-06 14:15:25 +0200

    Update add switch entity dialog with options for regulator locks and key locks (#18)
    * Add regulator and key lock to create switch dialog
    
    * Remove max length of entity name
    
    * Remove lock time and time unit

--- src/dialogs/lcn-create-entity-dialog.ts
+++ src/dialogs/lcn-create-entity-dialog.ts
@@ -93,7 +93,6 @@ export class CreateEntityDialog extends LitElement {
           id="name-input"
           label=${this.lcn.localize("name")}
           type="string"
-          maxLength="20"
           @input=${this._nameChanged}
         ></ha-textfield>
 
